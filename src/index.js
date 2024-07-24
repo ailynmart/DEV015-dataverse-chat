8baf4ccd63d5f13652a6c63b2d959a8c08952f03
Ailyn Martinez <ailyn.serrano09@gmail.com>
2024-07-24 07:22:52 -0600

    se incorporo lo del hito 1 que pide el readmi

--- src/index.js
+++ src/index.js
@@ -17,3 +17,25 @@ TODO:
 2.- Pasar "root element" a router.
 3.- Invocar el router para renderizar la vista correcta.
 */
+// src/index.js
+import { setRootEl, setRoutes, onURLChange } from './router';
+import Home from './views/Home';
+import Example from './views/Example.js';
+
+// Define las rutas de tu aplicación
+const routes = {
+  '/': Home,
+  // Agrega otras rutas aquí, por ejemplo:
+  // '/about': About,
+  // '/contact': Contact,
+};
+
+// Obtén el elemento raíz
+const rootElement = document.getElementById('root');
+
+// Configura el elemento raíz y las rutas
+setRootEl(rootElement);
+setRoutes(routes);
+
+// Gestiona la carga de la página inicial
+onURLChange();
